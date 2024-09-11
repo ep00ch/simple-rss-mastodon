73a43bc14dd24ead94ea4f5ba101762e602af5d9
Eric Pooch <EPooch@sandiego.gov>
2024-09-11 07:42:43 -0700

    Fix wrong settings and race condition for multiple feeds

--- simple-rss.js
+++ simple-rss.js
@@ -1,55 +1,53 @@
 var simpleRSSPlugin = (function() {
 	// get all the feed containers
 	var feedsNodes = document.querySelectorAll('[data-rss-feed]');
-	// Convert to array
-	var feeds = [].slice.call(feedsNodes);
-	for (var i = 0; i < feeds.length; i++) {
-		var container = feedsNodes[i];
-		
+
+	for (var container of feedsNodes) {
 		// get feed URL
-		var url = container.getAttribute('data-rss-feed');
-		// get whether to link titles
-		var addLink = container.getAttribute('data-rss-link-titles') || 'true';
-		
-		// get title wrapper element
-		var titleWrapper = container.getAttribute('data-rss-title-wrapper') || 'h2';
-		// Max outputs
-		var max = container.getAttribute('data-rss-max') || 10;
+		var url = encodeURIComponent(container.getAttribute('data-rss-feed'));
+
 		// Get data - append as script with callback to avoid CORS
 		var script = document.createElement('script');
-		script.src = document.location.protocol + '//api.rss2json.com/v1/api.json?callback=simpleRSSPlugin.handleJSON&rss_url=' + encodeURIComponent(url);
+		script.src = document.location.protocol + '//api.rss2json.com/v1/api.json?callback=simpleRSSPlugin.handleJSON&rss_url=' + url;
 		document.querySelector('head').appendChild(script);
-		
 		// Remove script
 		script.parentNode.removeChild(script);
 	}
+
 	// Callback function
-	var loops = 0;
 	function handleJSON(data) {
-		if (data.feed && data.items) {
-			
+		if (data.feed && data.items ) {
+			// find the div with the response data-rss-feed url
+			const container = document.querySelector('[data-rss-feed="' + data.feed.url + '"]');
+			// get whether to link titles
+			var addLink = container.getAttribute('data-rss-link-titles') || 'true';
+			// get title wrapper element
+			var titleWrapper = container.getAttribute('data-rss-title-wrapper') || 'h2';
+			// max number of feed items to show
+			var max = container.getAttribute('data-rss-max') || 10;
+
+			// New feed content container
 			var docFrag = document.createDocumentFragment();
-			for (var i = 0; i < data.items.length; i++) {
+
+			for (var i = 0; (i < data.items.length && i < max); i++) {
 				var e = data.items[i];
 				var tempNode = document.createElement('div');
+
 				var template = '<' + titleWrapper + '><a href="' + e.link + '">' + e.title + '</a></' + titleWrapper + '>' + e.content;
 				if (addLink === 'false') {
 					template = '<' + titleWrapper + '>' + e.title + '</' + titleWrapper + '>' + e.content;
 				}
-				if (i < max) {
-					
-					tempNode.innerHTML = template;
-					
-					docFrag.appendChild(tempNode);
-				}
+
+				tempNode.innerHTML = template;
+
+				docFrag.appendChild(tempNode);
 			}
-			container = feedsNodes[loops];
+
 			container.appendChild(docFrag);
-			loops++;
 		}
 	}
 	// Return function for use in global scope
 	return {
-		handleJSON:handleJSON
+		handleJSON: handleJSON
 	}
-})();
+}) ();
